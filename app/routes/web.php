15567a90fe07a5b03851295241d702d9bf52c155
Juan Galarraga <juan.galarraga@outlook.com.ar>
2024-05-14 21:47:25 -0300

    Añado vista de publications

--- app/routes/web.php
+++ app/routes/web.php
@@ -26,6 +26,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('publication')->group(function(){
+        Route::get('/create', function(){
+            return view('publications.create');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
