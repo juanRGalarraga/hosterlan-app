3deeb3b9c12380faa31e83cf1229a0787df6888e
nikillo18 <nicodrax18@gmail.com>
2024-05-20 19:36:47 -0300

    models echos

---
+++ app/app/Models/Publication.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publication extends Model
+{
+    use HasFactory;
+      protected $fillable = [
+        'price',
+        'ubication',
+        'description',
+        'room_count',
+        'pet',
+        'numbre_people'
+    ];
+}
