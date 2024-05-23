ce5c87765675c22e4a0ddcbe6cc6a5a4da70fe56
Juan Galarraga <juan.galarraga@outlook.com.ar>
2024-05-22 21:09:14 -0300

    Acomodar listado de publicaciones

--- app/resources/views/components/booking-card.blade.php
+++ app/resources/views/components/booking-card.blade.php
@@ -2,16 +2,17 @@
     $imageSourceDefault = 'https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80';
 @endphp
 
-@props(['imageSource' => $imageSourceDefault, 'title', 'description'])
+@props(['imageSource' => $imageSourceDefault, 'title', 'description', 'buttonText' ,'rating' => ''])
 
-<div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
-  <div class="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+<div class="relative flex max-w-[26rem] flex-col bg-blend-darken bg-clip-border text-gray-700 shadow-lg w-80 h-[20rem]">
+  <div class="relative mx-4 mt-4 overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
     <img
       src="{{$imageSource}}"
       alt="ui/ux review check"
+      class="rounded-md"
     />
     <div class="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
-    <button
+    <!-- <button
       class="!absolute top-4 right-4 h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-red-500 transition-all hover:bg-red-500/10 active:bg-red-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       type="button"
       data-ripple-dark="true"
@@ -27,12 +28,12 @@
           <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"></path>
         </svg>
       </span>
-    </button>
+    </button> -->
   </div>
   <div class="p-6">
     <div class="mb-3 flex items-center justify-between">
-      <h5 class="block font-sans text-xl font-medium leading-snug tracking-normal text-blue-gray-900 antialiased">
-        {{$title}}
+      <h5 class="block font-sans font-medium leading-snug tracking-normal text-white antialiased text-sm">
+        {{Str::limit($title, 29)}}
       </h5>
       <p class="flex items-center gap-1.5 font-sans text-base font-normal leading-relaxed text-blue-gray-900 antialiased">
         <svg
@@ -48,10 +49,10 @@
             clip-rule="evenodd"
           ></path>
         </svg>
-        5.0
+        {{$rating}}
       </p>
     </div>
-    <p class="block font-sans text-base font-light leading-relaxed text-gray-700 antialiased">
+    <p class="block font-sans text-xs font-light leading-relaxed text-white antialiased max-h-5">
       {{$description}}
     </p>
     <!-- <div class="group mt-8 inline-flex flex-wrap items-center gap-3">
@@ -153,16 +154,6 @@
       >
         +20
       </span>
-
     </div> -->
-
-  </div>
-  <div class="p-6 pt-3">
-    <button
-      class="block w-full select-none rounded-lg bg-pink-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button"
-      data-ripple-light="true">
-      Ver publicacion
-    </button>
   </div>
 </div>
--- app/resources/views/publications/index.blade.php
+++ app/resources/views/publications/index.blade.php
@@ -1,14 +1,21 @@
 <x-app-layout>
-    <div>
+
+    <x-slot:header>
+        {{__('Propiedades disponibles')}}
+    </x-slot:header>
+
+    <div class="flex flex-wrap justify-end">
     @forelse ($publications as $publication)
-        <x-booking-card :title="__($publication->title??'Title')" :description="__($publication->description)"></x-booking-card>
+        <x-booking-card class="ml-3" :title="__($publication->title??'Title')" :description="__($publication->description)" :buttonText="__('')"></x-booking-card>
     @empty
         <span class="text-danger">
             <strong>{{__('No se encontraron publicaciones')}}</strong>
         </span>
     @endforelse
     </div>
+    <div class="text-center justify-center flex flex-shrink-2">
+        {{$publications->links()}}
+    </div>
 
-    {{$publications->links()}}
 
 </x-app-layout>
