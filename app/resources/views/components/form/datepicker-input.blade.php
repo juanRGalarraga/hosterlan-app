e8ff426977f7ad60cf67e03cc91e17f1e83c9f2f
Juan Galarraga <juan.galarraga@outlook.com.ar>
2024-05-26 19:23:54 -0300

    Corregir estilo de daterange

--- app/resources/views/components/form/datepicker-input.blade.php
+++ app/resources/views/components/form/datepicker-input.blade.php
@@ -1,14 +1,16 @@
 <!-- To use this component we need to laod calendar.js plugin before -->
-<div>
+<div class="w-fit">
     <label for="availableDayFrom" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$label}}</label>
     <div {{ $attributes->merge(['class' => 'wrapper-datepicker']) }}>
-        <input data-class="black-theme rounded-lg"  data-datepicker/>
+        <input data-class="black-theme rounded-lg" class="w-full"  data-datepicker/>
     </div>
 </div>
 
 <style>
     .wrapper-datepicker input {
-        background-color: black !important;
-        border-radius: 9px !important;
+        background-color: rgb(55 65 81 / var(--tw-bg-opacity)) !important;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-style: none;
     }
 </style>
--- app/resources/views/publications/index.blade.php
+++ app/resources/views/publications/index.blade.php
@@ -8,64 +8,18 @@
         <link rel="stylesheet" type="text/css" href="/css/calendar/jsCalendar.darkseries.min.css">
     @endpush
 
+    @push('custom-css')
+        <link rel="stylesheet" type="text/css" href="/css/publications/index.css">
+    @endpush
+
     <x-slot:header>
         {{__('Propiedades disponibles')}}
     </x-slot:header>
 
     <div class="flex">
-        <div class="w-[20%] pl-3 mt-3">
 
-            <x-form.minimal-input label="Buscar" class="mb-3" type="text" id="buscador"></x-form.minimal-input>
+        @include('publications.index-filters')
 
-            <x-form.select-input id="" label="{{__('Ordenar por')}}" class="mb-3">
-                <x-form.select-input-option value="Opcion1">Opcion1</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion2">Opcion2</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion3">Opcion3</x-form.select-input-option>
-                <x-form.select-input-option value="Etc">Etc</x-form.select-input-option>
-            </x-form.select-input>
-
-            <x-form.label text="Disponibilidad"></x-form.label>
-            <div class="flex flex-row mb-3">
-                <x-form.datepicker-input :label="__('Desde')" class="text-white max-w-[50%]"></x-form.datepicker-input>
-                <x-form.datepicker-input :label="__('Hasta')" class="text-white max-w-[50%]"></x-form.datepicker-input>
-            </div>
-
-            <x-form.label text="Precio"></x-form.label>
-            <div class="flex flex-row mb-3">
-                <x-form.minimal-input class="mb-3 w-[50%]" type="text" id="" placeholder="Min."></x-form.minimal-input>
-                <span>a</span>
-                <x-form.minimal-input class="mb-3 w-[50%]" type="text" id="" placeholder="Máx."></x-form.minimal-input>
-            </div>
-
-            <x-form.select-input id="" label="{{__('Habitaciones')}}" class="mb-3">
-                <x-form.select-input-option value="Opcion1">Opcion1</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion2">Opcion2</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion3">Opcion3</x-form.select-input-option>
-                <x-form.select-input-option value="Etc">Etc</x-form.select-input-option>
-            </x-form.select-input>
-
-            <x-form.select-input id="" label="{{__('Baños')}}" class="mb-3">
-                <x-form.select-input-option value="Opcion1">Opcion1</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion2">Opcion2</x-form.select-input-option>
-                <x-form.select-input-option value="Opcion3">Opcion3</x-form.select-input-option>
-                <x-form.select-input-option value="Etc">Etc</x-form.select-input-option>
-            </x-form.select-input>
-
-            <x-form.select-input id="" label="{{__('Tipo de renta')}}" class="mb-3">
-                <x-form.select-input-option value="Rent1">Rent1</x-form.select-input-option>
-                <x-form.select-input-option value="Rent2">Rent2</x-form.select-input-option>
-                <x-form.select-input-option value="Rent3">Rent3</x-form.select-input-option>
-            </x-form.select-input>
-
-            <x-form.label text="Metros cuadrados"></x-form.label>
-            <div class="flex flex-row mb-3">
-                <x-form.minimal-input class="mb-3 w-[50%]" type="text" id="" placeholder="Min."></x-form.minimal-input>
-                <span>a</span>
-                <x-form.minimal-input class="mb-3 w-[50%]" type="text" id="" placeholder="Máx."></x-form.minimal-input>
-            </div>
-
-            <x-form.toggle-switch label="Permite mascotas"></x-form.toggle-switch>
-        </div>
         <div class="w-[80%] flex flex-wrap justify-center mt-3">
             @forelse($publications as $publication)
                 <div>
