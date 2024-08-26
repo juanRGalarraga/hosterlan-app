f7d9c3792f15d1bd351afa3034c26d9833db385a
Juan Galarraga <juan_dlp2@outlook.com>
2024-08-26 09:14:18 -0300

    Fix declaration error

--- app/app/Models/Picture.php
+++ app/app/Models/Picture.php
@@ -16,7 +16,7 @@ class Picture extends Model
       'publication_id'
     ];
 
-    const DEFAULT_PICTURE = env('DEFAULT_PICTURE', 'default.svg');
+    const DEFAULT_PICTURE = 'default.svg';
     
     public function publication(){
       return $this->belongsTo(Publication::class);
