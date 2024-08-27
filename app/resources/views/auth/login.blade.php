a5004862603de007915b486d8eb0ed8c461d59a5
Juan Galarraga <juan.galarraga@outlook.com.ar>
2024-08-27 20:44:19 -0300

    Position register and forgot password links

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -32,18 +32,27 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('¿Olvidaste tu contraseña?') }}
-                </a>
-            @endif
-
+        <div class="text-center">
             <x-primary-button class="ms-3">
                 {{ __('Ingresar') }}
             </x-primary-button>
-            <a class="ml-2 inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" href="{{ route('register-type-user') }}">
-                {{ __('Registrasre') }}
+        </div>
+
+        <div class="border-t border-gray-600 my-4"></div>
+
+        <div class="text-center mt-4">
+            @if (Route::has('password.request'))
+                <span class="text-gray-400">{{ __('¿Olvidaste tu contraseña?') }}</span>
+                <a class="underline text-sm text-gray-600 dark:text-white hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Recuperar contraseña') }}
+                </a>
+            @endif
+        </div>
+
+        <div class="text-center mt-4">
+            <span class="text-gray-400">{{ __('¿No tienes una cuenta?') }}</span>
+            <a class="underline text-sm text-gray-600 dark:text-white hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register-type-user') }}">
+            {{ __('Registrarse') }}
             </a>
         </div>
     </form>
