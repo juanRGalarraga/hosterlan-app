56dbba690dab55f7cda1d9f5c552f254ed2e302a
Juan Galarraga <juan.galarraga@outlook.com.ar>
2024-09-10 17:40:50 -0300

    View reservation

--- app/resources/views/publications/show/main.blade.php
+++ app/resources/views/publications/show/main.blade.php
@@ -60,75 +60,76 @@
           {{__('Guardar')}}
         </button>
       </div>
-      </div>
+    </div>
 
-
-      <!-- Descripción -->
-      <div class="text-gray-300 rounded-lg bg-gray-500 p-5 mt-3">
-        <p id="textDescriptionContent">
-          {{$publication->description}}
-        </p>
-      </div>
-        
-      <hr class="my-3">
-
-      <p class="text-sm text-center mb-3 dark:text-white">{{__('Días disponibles')}}</p>
+    <p class="text-sm text-center mb-3 dark:text-white">{{__('Días disponibles')}}</p>
       
     <div class="w-full text-center text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         @foreach ($publication->daysAvailable as $dayAvailable)
-              @php
+            @php
                 $isMyReservation = $dayAvailable->isMyReservation();
                 $isAvailable = $dayAvailable->isAvailable();
                 $canReserve = !$isMyReservation && $isAvailable;
                 $label = $isMyReservation ? __('Solicitado') : '';
-              @endphp
+            @endphp
 
-              <button type="button"
+            <button type="button"
                 data-day-available-id={{$dayAvailable->id}} 
                 data-date="{{$dayAvailable->since . " hasta " . $dayAvailable->to}}"
                 data-modal-target="reserveDayModal"
                 data-modal-show="reserveDayModal"
-                  @class([
-                      'buttons-reserve-day',
-                      'cursor-not-allowed' => !$canReserve,
-                      'relative',
-                      'inline-flex',
-                      'items-center',
-                      'w-full',
-                      'px-4',
-                      'py-2',
-                      'text-sm',
-                      'font-medium',
-                      'border-b',
-                      'border-gray-200',
-                      'rounded-t-lg',
-                      'hover:bg-gray-100' => $canReserve,
-                      'hover:text-blue-700'  => $canReserve,
-                      'focus:z-10 focus:ring-2'  => $canReserve,
-                      'focus:ring-blue-700'  => $canReserve,
-                      'focus:text-blue-700'  => $canReserve,
-                      'dark:border-gray-600',
-                      'dark:hover:text-gray-500' => $canReserve,
-                      'dark:focus:text-white'  => $canReserve,
-                      'dark:focus:ring-gray-500'  => $canReserve,
-                      'cursor-pointer'  => $canReserve,
-                      'text-gray-500' => !$canReserve,
-                  ])
-                  @disabled(!$canReserve)
-                  type="button">
+                @class([
+                    'buttons-reserve-day',
+                    'cursor-not-allowed' => !$canReserve,
+                    'relative',
+                    'inline-flex',
+                    'items-center',
+                    'w-full',
+                    'px-4',
+                    'py-2',
+                    'text-sm',
+                    'font-medium',
+                    'border-b',
+                    'border-gray-200',
+                    'rounded-t-lg',
+                    'hover:bg-gray-100' => $canReserve,
+                    'hover:text-blue-700'  => $canReserve,
+                    'focus:z-10 focus:ring-2'  => $canReserve,
+                    'focus:ring-blue-700'  => $canReserve,
+                    'focus:text-blue-700'  => $canReserve,
+                    'dark:border-gray-600',
+                    'dark:hover:text-gray-500' => $canReserve,
+                    'dark:focus:text-white'  => $canReserve,
+                    'dark:focus:ring-gray-500'  => $canReserve,
+                    'cursor-pointer'  => $canReserve,
+                    'text-gray-500' => !$canReserve,
+                ])
+                @disabled(!$canReserve)
+                type="button">
 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
-                  </svg>
-                  {{" $dayAvailable->since " . __('hasta el') . " {$dayAvailable->to} "}}
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                </svg>
+                {{" $dayAvailable->since " . __('hasta el') . " {$dayAvailable->to} "}}
 
-                  @if ($label)
-                     <x-utils.badge class="ml-2">{{$label}}</x-utils.badge>
-                  @endif
+                @if ($label)
+                    <x-utils.badge class="ml-2">{{$label}}</x-utils.badge>
+                @endif
 
-              </button>
+            </button>
         @endforeach
     </div>
+
+    <hr class="my-3">
+    
+    <!-- Descripción -->
+    <div class="text-gray-300 rounded-lg bg-gray-500 p-5 mt-3">
+        <p id="textDescriptionContent">
+          {{$publication->description}}
+        </p>
+    </div>
+        
+      
   </div>
 </aside>
 
