cc3b92fd07f622f3e4f030254091bb768291a063
Juan Galarraga <juan_dlp2@outlook.com>
2024-09-26 14:18:05 -0300

    Fix gap y

--- app/resources/views/publications/index/card-list.blade.php
+++ app/resources/views/publications/index/card-list.blade.php
@@ -3,11 +3,11 @@
 @endphp
 @if($publications->count() >= 1)
 <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 min-h-full">
-    <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-x-2 lg:grid-cols-2 xl:grid-cols-4 grid-flow-row">
+    <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-32 lg:grid-cols-2 xl:grid-cols-4 grid-flow-row">
         @foreach($publications as $publication)
             <div class="mx-auto">
-                <x-booking-card id="{{$publication->id}}" class="hover:cursor-pointer clickeable-card w-[300px]  md:w-[200px] lg:w-[350px]" :imageSource="$publication->getFirstPicture()" :title="__($publication->title)" :description="__($publication->description)" :buttonText="__('')"></x-booking-card>
-            </div>    
+                <x-booking-card id="{{$publication->id}}" class="hover:cursor-pointer clickeable-card w-[300px]  md:w-[200px] lg:w-[350px] min-h-fit" :imageSource="$publication->getFirstPicture()" :title="__($publication->title)" :description="__($publication->description)" :buttonText="__('')"></x-booking-card>
+            </div>
         @endforeach
     </div>
 </div>
